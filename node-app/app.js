eb8c9b4eef0e9de72a6c5a5a1760d153fc5fb9fd
Tilekbek(Taylor) Kadyrov <tkadyrov@rcgdirect.com>
2019-08-15 16:39:41 -0500

    notes

--- node-app/app.js
+++ node-app/app.js
@@ -1,57 +1,57 @@
-const yargs = require('yargs');
+const yargs = require("yargs");
+const notes = require("./notes");
 
 // customize yargs version
-yargs.version('1.1.0');
+yargs.version("1.1.0");
 
 // create add command
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        },
-        body: {
-            describe: 'Note body',
-            demandOption: true,
-            type: 'string'
-        }
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
     },
-    handler: function (argv) {
-        console.log('Title: ' + argv.title);
-        console.log('Body: ' + argv.body);
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string"
     }
-})
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body);
+  }
+});
 
 // create remove command
 yargs.command({
-    command: 'remove',
-    describe: 'Remove a note',
-    handler: function () {
-        console.log('Removing note!')
-    }
-})
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing note!");
+  }
+});
 
 // create read command
 
 yargs.command({
-    command: 'read',
-    describe: 'read note',
-    handler: function () {
-        console.log('Reading a note!')
-    }
-})
+  command: "read",
+  describe: "read note",
+  handler: function () {
+    console.log("Reading a note!");
+  }
+});
 
 // create list command
 yargs.command({
-    command: 'list',
-    describe: 'list note',
-    handler: function () {
-        console.log('Listing a note!')
-    }
-})
+  command: "list",
+  describe: "list note",
+  handler: function () {
+    console.log("Listing a note!");
+  }
+});
 
 // add, remove, read, list
 yargs.parse();
--- node-app/notes.js
+++ node-app/notes.js
@@ -1,5 +1,46 @@
+const fs = require("fs");
+
 const getNotes = function () {
-    return 'Your notes...'
+  return "Your notes...";
+};
+
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title
+  })
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    })
+    saveNotes(notes);
+    console.log('New note added')
+  } else {
+    console.log('Note title take!')
+  }
+
+};
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
 }
 
-module.exports = getNotes;
+const loadNotes = function () {
+
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return []
+  }
+
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+};
