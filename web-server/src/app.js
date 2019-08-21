004efc7dde8e50e242262bc32e1097a29433c504
Tilekbek(Taylor) Kadyrov <tkadyrov@rcgdirect.com>
2019-08-21 15:13:30 -0500

    express app get/use/render

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,45 +1,40 @@
+const path = require('path');
 const express = require('express');
 
 const app = express()
 
+// define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
 app.get('', (req, res) => {
-    res.send('<h1>Hello Express</h1>');
+    res.render('index', {
+        title: 'Weather',
+        name: 'Tilek Kadyrov'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Tilek Kadyrov'
+    })
 })
 
 app.get('/help', (req, res) => {
-    res.send([
-        {
-            name: 'Tilek',
-        },
-        {
-            name: 'Bermet'
-        }
-    ]);
+    res.render('help', {
+        message: 'hello this is help page rendering!'
+    })
 })
 
-// Goal. Update routes.
-// 1. Setup about route to render a title with HTML
-// 2 Setup a weather route to send back JSON
-//   - Object with forecast and location strings
-// 3. Test your work by visiting both in the browser
-
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About page</h1>');
-})
-
-app.get('/weather', (req, res) => {
-    res.send({
-        location: 'Bishkek',
-        summary: 'Cloudly',
-        temperature: 65
-    });
-})
-
-// app.com
-// app.com/help
-// app.com/about
-
+// listening part #3000 
 app.listen(3000, () => {
     console.log('Server started on 3000');
 }); 
